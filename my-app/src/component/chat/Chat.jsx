0399d0572d07c7f27d05c27633fc00c2ca0bd329
sompadidvirada <sompadidppp@gmail.com>
2025-04-30 17:01:23 +0700

    chat font end

--- my-app/src/component/chat/Chat.jsx
+++ my-app/src/component/chat/Chat.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
-import "./chat.css"
-
+import React, { useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
-  return (
-    <div className='chat'>Chat</div>
-  )
-}
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
 
-export default Chat
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+  };
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jonhny Sin</span>
+            <p>Lorem want to write something about you in the long time ago.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png " alt="" />
+          <img src="./info.png " alt="" />
+        </div>
+      </div>
+      <div className="center">
+
+
+
+
+
+
+
+        
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message...."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
